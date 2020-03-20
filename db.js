41203f551ea9a4832dda12342cf9d04b13214597
sesandrayoo <sesandrayoo@gmail.com>
2020-03-19 20:59:36 -0700

    Separate DB connection from queries

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+// Create connection
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'RateMyLandlord'
+});
+
+// Connect to the MySQL Database
+connection.connect((err) => {
+    if(err){
+        throw err;
+    }
+    console.log('MySql Connected...');
+});
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,21 +1,5 @@
 const express = require('express');
-const mysql = require('mysql');
-
-// Create connection
-const db = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'RateMyLandlord'
-});
-
-// Connect to the MySQL Database
-db.connect((err) => {
-    if(err){
-        throw err;
-    }
-    console.log('MySql Connected...');
-});
+var db = require('./db');
  
 const app = express(); 
 app.set('views', `${__dirname}/views`)
