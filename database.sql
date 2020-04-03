6b90d39636dc8b2291206f421fe61038a74d3823
sesandrayoo <sesandrayoo@gmail.com>
2020-04-03 14:12:06 -0700

    Make IDs AUTO_INCREMENT

--- database.sql
+++ database.sql
@@ -10,20 +10,20 @@ USE RateMyLandlord;
 /********* ADMIN TABLE **********/
 /********************************/
 CREATE TABLE Admin (
-	adminID varchar(50) PRIMARY KEY,
+	adminID INT PRIMARY KEY AUTO_INCREMENT,
 	name varchar(100)
 );
 
 /*** Admin: Insert ***/
-INSERT INTO Admin VALUES('admin1', 'Sandra');
-INSERT INTO Admin VALUES('admin2', 'Marshal');
-INSERT INTO Admin VALUES('admin3', 'Yuxiang');
+INSERT INTO Admin (name) VALUES('Sandra');
+INSERT INTO Admin (name) VALUES('Marshal');
+INSERT INTO Admin (name) VALUES('Yuxiang');
 
 /********************************/
 /********* USER TABLE **********/
 /********************************/
 CREATE TABLE User (
-	userID VARCHAR(50) PRIMARY KEY,
+	userID INT PRIMARY KEY AUTO_INCREMENT,
 	userName VARCHAR(100),
 	userAbout VARCHAR(500),
 	userType VARCHAR(50),
@@ -31,66 +31,66 @@ CREATE TABLE User (
 );
 
 /*** User: Insert ***/
-INSERT INTO User VALUES('user1', 'JimHalpert', 'Paper salesman', 'landlord', 'password123');
-INSERT INTO User VALUES('user2', 'PamBH', 'Office Admin, I have 2 kids.', 'landlord', 'password123');
-INSERT INTO User VALUES('user3', 'KevMalone', 'Accountant', 'tenant', 'password');
-INSERT INTO User VALUES('user4', 'ErinH', 'Secretary', 'tenant', 'password');
-INSERT INTO User VALUES('user5', 'MichaelScott', 'I manage people. I have a condo.', 'landlord', 'password');
-INSERT INTO User VALUES('user6', 'DwightKS', 'Proud assistant to the regional manager. Owner of a large beet farm.', 'landlord', 'battlestargalactica');
-INSERT INTO User VALUES('user7', 'OscarM', 'I like a tidy apartment', 'tenant', 'password');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('JimHalpert', 'Paper salesman', 'landlord', 'password123');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('PamBH', 'Office Admin, I have 2 kids.', 'landlord', 'password123');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('KevMalone', 'Accountant', 'tenant', 'password');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('ErinH', 'Secretary', 'tenant', 'password');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('MichaelScott', 'I manage people. I have a condo.', 'landlord', 'password');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('DwightKS', 'Proud assistant to the regional manager. Owner of a large beet farm.', 'landlord', 'battlestargalactica');
+INSERT INTO User (userName, userAbout, userType, userPassword) VALUES('OscarM', 'I like a tidy apartment', 'tenant', 'password');
 
 
 /********************************/
 /********* TENANT TABLE **********/
 /********************************/
 CREATE TABLE Tenant (
-	userID VARCHAR(50) PRIMARY KEY NOT NULL,
+	userID INT PRIMARY KEY NOT NULL,
 	FOREIGN KEY (userID) REFERENCES
 		User(userID)
 	ON DELETE CASCADE
 );
 
 /*** Tenant: Insert ***/
-INSERT INTO Tenant VALUES('user3');
-INSERT INTO Tenant VALUES('user4');
-INSERT INTO Tenant VALUES('user7');
+INSERT INTO Tenant VALUES(3);
+INSERT INTO Tenant VALUES(4);
+INSERT INTO Tenant VALUES(7);
 
 
 /********************************/
 /********* LANDLORD TABLE (not to be confused with LandlordProfile Table) **********/ 
 /********************************/
 CREATE TABLE Landlord (
-	userID VARCHAR(50) PRIMARY KEY NOT NULL,
+	userID INT PRIMARY KEY NOT NULL,
 	FOREIGN KEY (userID) REFERENCES
 		User(userID)
 	ON DELETE CASCADE
 );
 
 /*** Landlord: Insert ***/
-INSERT INTO Landlord VALUES('user1');
-INSERT INTO Landlord VALUES('user2');
-INSERT INTO Landlord VALUES('user5');
-INSERT INTO Landlord VALUES('user6');
+INSERT INTO Landlord VALUES(1);
+INSERT INTO Landlord VALUES(2);
+INSERT INTO Landlord VALUES(5);
+INSERT INTO Landlord VALUES(6);
 
 /*******************************************/
 /********* LANDLORD PROFILE TABLE **********/
 /*******************************************/
 CREATE TABLE LandlordProfile (
-	profileID VARCHAR(50) PRIMARY KEY,
+	profileID INT PRIMARY KEY AUTO_INCREMENT,
     profileName VARCHAR(75) NOT NULL,
 	profileCity VARCHAR(500),
-	userID VARCHAR(50),
+	userID INT,
 	FOREIGN KEY (userID) REFERENCES
 		User(userID) 
 	ON DELETE SET NULL
 );
 
 /*** LandlordProfile: Insert ***/
-INSERT INTO LandlordProfile VALUES('profile1', 'David Wallace', 'Vancouver', 'user3');
-INSERT INTO LandlordProfile VALUES('profile2', 'Robert Dunder', 'Vancouver', 'user3');
-INSERT INTO LandlordProfile VALUES('profile3', 'Jan Levinson', 'Victoria', 'user7');
-INSERT INTO LandlordProfile VALUES('profile4', 'Creed Bratton', 'Vancouver', 'user4');
-INSERT INTO LandlordProfile VALUES('profile5', 'Bob Vance', 'Vancouver', 'user4');
+INSERT INTO LandlordProfile (profileName, profileCity, userID) VALUES('David Wallace', 'Vancouver', 3);
+INSERT INTO LandlordProfile (profileName, profileCity, userID) VALUES('Robert Dunder', 'Vancouver', 3);
+INSERT INTO LandlordProfile (profileName, profileCity, userID) VALUES('Jan Levinson', 'Victoria', 7);
+INSERT INTO LandlordProfile (profileName, profileCity, userID) VALUES('Creed Bratton', 'Vancouver', 4);
+INSERT INTO LandlordProfile (profileName, profileCity, userID) VALUES('Bob Vance', 'Vancouver', 4);
 
 /************************************************************/
 /********* MUNICIPALITY TABLE (sub table of property) *******/
@@ -99,7 +99,7 @@ INSERT INTO LandlordProfile VALUES('profile5', 'Bob Vance', 'Vancouver', 'user4'
 CREATE TABLE Municipality(
 	propertyPostal VARCHAR(10) PRIMARY KEY,
 	propertyCity VARCHAR(100)
-);
+); 
 
 /*** Municipality: Insert ***/
 INSERT INTO Municipality VALUES('V8G3G5', 'Vancouver');
@@ -131,7 +131,7 @@ INSERT INTO PropertyLocation VALUES('456 2nd Ave', 'V3Q3D6');
 /************** PROPERTY TABLE *************/
 /*******************************************/
 CREATE TABLE Property (
-	propertyID VARCHAR(50) PRIMARY KEY, 
+	propertyID INT PRIMARY KEY AUTO_INCREMENT, 
 	propertyAvailability BOOL, 
 	propertyStreetAddress VARCHAR(100), 
 	propertyDescription VARCHAR(500), 
@@ -144,19 +144,19 @@ CREATE TABLE Property (
 );
 
 /*** Property: Insert ***/
-INSERT INTO Property VALUES('property1', 1, '789-15918 24 Ave', 'A spacious downtown condo, newly renovated.', 1, 2, 'condo');
-INSERT INTO Property VALUES('property2', 0, '456 2nd Ave', 'Perfect for a single person. A nice property.', 1, null, 'condo');
-INSERT INTO Property VALUES('property3', 0, '1234 West 6th Ave', 'FAMILY TOWNHOUSE. Close to schools.', 2.5, 4, 'townhouse');
-INSERT INTO Property VALUES('property4', 1, '987 Sesame Street', 'Close to skytrain. Kinda old.', null, null, 'detached house');
-INSERT INTO Property VALUES('property5', 1, '3312 Agar Street', 'Bright place. Pets allowed!', '2', 2, 'townhouse');
+INSERT INTO Property (propertyAvailability, propertyStreetAddress, propertyDescription, Bathrooms, Bedrooms, propertyType) VALUES(1, '789-15918 24 Ave', 'A spacious downtown condo, newly renovated.', 1, 2, 'condo');
+INSERT INTO Property (propertyAvailability, propertyStreetAddress, propertyDescription, Bathrooms, Bedrooms, propertyType) VALUES(0, '456 2nd Ave', 'Perfect for a single person. A nice property.', 1, null, 'condo');
+INSERT INTO Property (propertyAvailability, propertyStreetAddress, propertyDescription, Bathrooms, Bedrooms, propertyType) VALUES(0, '1234 West 6th Ave', 'FAMILY TOWNHOUSE. Close to schools.', 2.5, 4, 'townhouse');
+INSERT INTO Property (propertyAvailability, propertyStreetAddress, propertyDescription, Bathrooms, Bedrooms, propertyType) VALUES(1, '987 Sesame Street', 'Close to skytrain. Kinda old.', null, null, 'detached house');
+INSERT INTO Property (propertyAvailability, propertyStreetAddress, propertyDescription, Bathrooms, Bedrooms, propertyType) VALUES(1, '3312 Agar Street', 'Bright place. Pets allowed!', '2', 2, 'townhouse');
 
 
 /*************************************/
 /************** OWNS *****************/
 /*************************************/
 CREATE TABLE Owns (
-	profileID VARCHAR(50) NOT NULL,
-	propertyID VARCHAR(50) NOT NULL,
+	profileID INT NOT NULL,
+	propertyID INT NOT NULL,
 	PRIMARY KEY (profileID, propertyID),
 	FOREIGN KEY (profileID) REFERENCES
 	LandlordProfile(profileID)
@@ -167,18 +167,18 @@ CREATE TABLE Owns (
 );
 
 /*** Owns: Insert ***/
-INSERT INTO Owns VALUES('profile2', 'property1');
-INSERT INTO Owns VALUES('profile1', 'property2');
-INSERT INTO Owns VALUES('profile4', 'property3');
-INSERT INTO Owns VALUES('profile3', 'property4');
-INSERT INTO Owns VALUES('profile5', 'property5');
+INSERT INTO Owns VALUES(2, 1);
+INSERT INTO Owns VALUES(1,2);
+INSERT INTO Owns VALUES(4,3);
+INSERT INTO Owns VALUES(3,4);
+INSERT INTO Owns VALUES(5,5);
 
 /****************************************/
 /************** LISTING *****************/
 /****************************************/
 CREATE TABLE Listing (
-	listingID VARCHAR(50) PRIMARY KEY,
-	uploaderID VARCHAR(50),
+	listingID INT PRIMARY KEY AUTO_INCREMENT,
+	uploaderID INT,
 	listingTitle VARCHAR(150),
 	listingArea VARCHAR(50),
 	listingDescription VARCHAR(500),
@@ -189,18 +189,18 @@ CREATE TABLE Listing (
 );
 
 /*** Listing: Insert ***/
-INSERT INTO Listing VALUES('listing1', 'user1', 'New Apartment for Rent', 'Vancouver', 'Close to skytrain, brand new!', '$800/month');
-INSERT INTO Listing VALUES('listing2', 'user2', 'Downtown Condo for Rent!', 'Vancouver', 'Walking distance to mall', '$1200/month');
-INSERT INTO Listing VALUES('listing3', 'user6', 'Townhouse Available', 'Vancouver', 'Peaceful mountain views', '$2500/month');
+INSERT INTO Listing (uploaderID, listingTitle, listingArea, listingDescription, listingPrice) VALUES(1, 'New Apartment for Rent', 'Vancouver', 'Close to skytrain, brand new!', '$800/month');
+INSERT INTO Listing (uploaderID, listingTitle, listingArea, listingDescription, listingPrice) VALUES(2, 'Downtown Condo for Rent!', 'Vancouver', 'Walking distance to mall', '$1200/month');
+INSERT INTO Listing (uploaderID, listingTitle, listingArea, listingDescription, listingPrice) VALUES(6, 'Townhouse Available', 'Vancouver', 'Peaceful mountain views', '$2500/month');
 
 
 /*********************************************/
-/************** REVIEW *****************/
+/****************** REVIEW *******************/
 /*********************************************/
 CREATE TABLE Review (
-	reviewID VARCHAR(50) PRIMARY KEY,
-	userID VARCHAR(50),
-	text VARCHAR(1024),
+	reviewID INT PRIMARY KEY AUTO_INCREMENT,
+	userID INT,
+	reviewText VARCHAR(1024),
     starRating INT,
 	FOREIGN KEY (userID) REFERENCES 
 		User(userID)
@@ -208,18 +208,18 @@ CREATE TABLE Review (
  );
 
 /*** Review: Insert ***/
-INSERT INTO Review VALUES('review1', 'user3', 'BEST. LANDLORD. EVER. They are soooo personable and super understanding. Very responsive too.', 5);
-INSERT INTO Review VALUES('review2', 'user4', 'Pretty reasonable landlord.', 4);
-INSERT INTO Review VALUES('review3', 'user6', 'Holy moly this landlord is crap. If I could give a negative 10 stars I would.', 1);
+INSERT INTO Review (userID, reviewText, starRating) VALUES(3, 'BEST. LANDLORD. EVER. They are soooo personable and super understanding. Very responsive too.', 5);
+INSERT INTO Review (userID, reviewText, starRating)  VALUES(4, 'Pretty reasonable landlord.', 4);
+INSERT INTO Review (userID, reviewText, starRating)  VALUES(6, 'Holy moly this landlord is crap. If I could give a negative 10 stars I would.', 1);
 
 
 /*********************************************/
 /************** VERIFICATION *****************/
 /*********************************************/
 CREATE TABLE Verification (
-	verificationID VARCHAR(50),
-    reviewID VARCHAR(50),
-	adminID VARCHAR(50),
+	verificationID INT AUTO_INCREMENT,
+    reviewID INT,
+	adminID INT,
 	verificationStatus BOOL,
 	document VARCHAR(255),
 	PRIMARY KEY (verificationID, adminID),
@@ -228,15 +228,15 @@ CREATE TABLE Verification (
  );
 
 /*** Verification: Insert ***/
-INSERT INTO Verification VALUES('verification1', 'review1', 'admin1', 1, 'contracts');
-INSERT INTO Verification VALUES('verification2', 'review2', 'admin1', 0, 'contracts');
-INSERT INTO Verification VALUES('verification3', 'review3', 'admin2', 1, 'contracts');
+INSERT INTO Verification (reviewID, adminID, verificationStatus, document) VALUES(1, 1, 1, 'contracts');
+INSERT INTO Verification (reviewID, adminID, verificationStatus, document) VALUES(2, 1, 0, 'contracts');
+INSERT INTO Verification (reviewID, adminID, verificationStatus, document) VALUES(3, 2, 1, 'contracts');
 
 /*********************************************/
 /************** VERIFICATION_LOG (claims sub-table) *****************/
 /*********************************************/
 CREATE TABLE Verification_Log(
-	verificationID VARCHAR(50) PRIMARY KEY,
+	verificationID INT PRIMARY KEY,
 	claimDateTime DATE NOT NULL,
 	FOREIGN KEY (verificationID) REFERENCES 
 		Verification(verificationID)
@@ -244,15 +244,15 @@ CREATE TABLE Verification_Log(
 );
 
 /*** Verification: Insert ***/
-INSERT INTO Verification_Log VALUES('verification1', '2019-06-15');
+INSERT INTO Verification_Log VALUES(1, '2019-06-15');
 
 /*********************************************/
 /************** VERIFICATION_CLAIM (claims sub-table) *****************/
 /*********************************************/
 CREATE TABLE Verification_Claim(
-	profileID VARCHAR(50),
-	userID VARCHAR(50),
-	verificationID VARCHAR(50),
+	profileID INT,
+	userID INT,
+	verificationID INT,
 	PRIMARY KEY (profileID, userID),
 	FOREIGN KEY (profileID) REFERENCES 
 		LandlordProfile(profileID)
@@ -263,7 +263,7 @@ CREATE TABLE Verification_Claim(
 );
 
 /*** Verification: Insert ***/
-INSERT INTO Verification_Claim VALUES('profile1', 'user3', 'verification1');
+INSERT INTO Verification_Claim VALUES(1, 3, 1);
 
 
 
